259cf0649c89bf51dfc465b4d1f01d42b6fecbec
Chernyshelly <andreycherni@gmail.com>
2021-06-08 22:22:42 +0300

    Made project compile Added empty TeacherController Added teacher model added EF5 and Application context for it

---
+++ server/WebApi/Models/DBManager/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Models.DBManager
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Teacher> Teachers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=Keymaster;Username=postgres;Password=postgres");
+        }
+    }
+}

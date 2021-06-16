098b190d8125e74eeefeb014cc2f8cd9429482ca
SoldatovMikhaul <misha.soldatov.96@mail.ru>
2021-06-16 18:30:35 +0300

    added header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import { render } from '@testing-library/react';
 import MyTable from './components/MyTable'
+import MyHeader from './components/MyHeader'
 
 class App extends React.Component{
   render(){
     return(
       <>
+        <MyHeader/>
         <MyTable/>
       </>
     );

024fae609538ac37d5302b7d8cfcca578ab65c0e
Chernyshelly <andreycherni@gmail.com>
2021-06-23 16:16:48 +0300

    Key class added

---
+++ server/Domain/Models/Key.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Models
+{
+    public class Key
+    {
+        public int Id { get; set; }
+
+        public string Classroom { get; set; }
+
+        public string QR { get; set; }
+
+        public Teacher Teacher { get; set; }
+    }
+}

e6d2b43b2e05ac71e0f2ff84f6bd3768c6c19b19
Chernyshelly <andreycherni@gmail.com>
2021-06-23 17:04:09 +0300

    KeyRepository and CreateRequestDto added

---
+++ server/Application/DTO/Request/KeyCreateRequestDto.cs
@@ -0,0 +1,24 @@
+﻿namespace Application.DTO.Request
+{
+    using Application.Interfaces;
+    using Domain.Models;
+
+    public class KeyCreateRequestDto : IDtoMapper<Key>
+    {
+        public string Classroom { get; set; }
+
+        public string QR { get; set; }
+
+        public Teacher Teacher { get; set; }
+
+        public Key ToModel()
+        {
+            return new Key()
+            {
+                Classroom = this.Classroom,
+                QR = this.QR,
+                Teacher = this.Teacher,
+            };
+        }
+    }
+}

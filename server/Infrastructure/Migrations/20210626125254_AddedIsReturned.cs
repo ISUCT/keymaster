69e80a1081ff5719ee03efdde65ac5208fa1d7b9
Chernyshelly <andreycherni@gmail.com>
2021-06-26 15:53:27 +0300

    AddedMigration

---
+++ server/Infrastructure/Migrations/20210626125254_AddedIsReturned.cs
@@ -0,0 +1,24 @@
+﻿namespace Infrastructure.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    public partial class AddedIsReturned : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsReturned",
+                table: "Assignments",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsReturned",
+                table: "Assignments");
+        }
+    }
+}
